e94ec165b77c5e1a0af44fa01600b21838dc557e
narayanasetti.s <narayanasetti.srinivasarao@yatra.com>
2019-10-19 16:22:00 +0530

    added initial files

---
+++ src/components/header-content.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class HeaderContent extends React.Component {
+    render() {
+        return (
+        <h3>ATM Machine</h3>
+    )
+ } 
+}
+
+export default HeaderContent;
